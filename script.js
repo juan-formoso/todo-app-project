b540aed524b4273e7655340bef4420c027c7626f
Juan Formoso <trajano.juan@gmail.com>
2021-07-14 00:38:18 -0400

    Requisitos 5 e 6

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const createButton = document.getElementById('criar-tarefa');
 
+function addItem(item) {
+  const list = document.getElementById('lista-tarefas');
+  const li = document.createElement('li');
+  li.textContent = item;
+  list.appendChild(li);
+}
+
+createButton.addEventListener('click', () => {
+  const input = document.getElementById('texto-tarefa');
+  addItem(input.value);
+  input.value = '';
+});
