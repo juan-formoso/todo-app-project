a3c743927f2a8cb8b55e3da8d24370d53616b526
Juan Formoso <trajano.juan@gmail.com>
2021-07-14 00:58:31 -0400

    Tentando finalizar requisito 7

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const createButton = document.getElementById('criar-tarefa');
+const itens = document.getElementById('lista-tarefas');
 
 function addItem(item) {
   const list = document.getElementById('lista-tarefas');
@@ -11,4 +12,17 @@ createButton.addEventListener('click', () => {
   const input = document.getElementById('texto-tarefa');
   addItem(input.value);
   input.value = '';
+});
+
+function removeItem() {
+  const selectedItem = document.getElementsByClassName('selected');
+  if (selectedItem != null) {
+    selectedItem.classList.remove('selected');
+  }
+}
+
+itens.addEventListener('click', (item) => {
+  const itemList = item.target;
+  removeItem();
+  itemList.classList.add('selected');
 });
